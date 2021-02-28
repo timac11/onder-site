364d2c2b2ef3a138cf09001b07e7805d46f5bc55
Nikita Izrailev <izrailev@phystech.edu>
2021-02-28 16:17:44 +0300

    added menu

---
+++ assets/js/menu.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var menuButton = document.querySelector(".menu button");
+  menuButton.onclick = handleClick;
+
+  var menuElements = document.querySelectorAll(".menu__list > li");
+  menuElements.forEach(function (element) {
+    element.onclick = handleClick;
+  });
+
+  initClickOutside();
+});
+
+function handleClick() {
+  var element = document.querySelector(".menu__list");
+  if (element.classList.contains("menu__list_visible")) {
+    close();
+  } else {
+    open();
+  }
+}
+
+function initClickOutside() {
+  document.addEventListener("click", function (event) {
+    var element = document.querySelector("header");
+    if (event.target !== element && !element.contains(event.target)) {
+      close();
+    }
+  });
+}
+
+function open() {
+  document.querySelector(".menu__list").classList.replace("menu__list_hidden", "menu__list_visible");
+}
+
+function close() {
+  document.querySelector(".menu__list").classList.replace("menu__list_visible", "menu__list_hidden");
+}
